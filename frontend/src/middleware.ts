c51eb8482bd19116796cddfeb1eb443f034ef183
Walter-D3v <walterantoniomelendez10@gmail.com>
2026-04-13 16:06:36 -0600

    cors update

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -25,7 +25,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - Imágenes y assets estáticos públicos (.png, .jpg, .svg, etc.)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
